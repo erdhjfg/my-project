babacfd8cf3e1c808e62e6d115616dfe35a9a0f2
erdhjfg <121865822+erdhjfg@users.noreply.github.com>
2023-01-04 18:19:06 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,91 @@
+function Err () {
+    images.createBigImage(`
+        # # # . . . . . . .
+        # . . . . . . . . .
+        # # # . # # . # # .
+        # . . . # . . # . .
+        # # # . # . . # . .
+        `).showImage(0)
+    images.createBigImage(`
+        # # # . . . . . . .
+        # . . . . . . . . .
+        # # # . # # . # # .
+        # . . . # . . # . .
+        # # # . # . . # . .
+        `).showImage(1)
+    images.createBigImage(`
+        # # # . . . . . . .
+        # . . . . . . . . .
+        # # # . # # . # # .
+        # . . . # . . # . .
+        # # # . # . . # . .
+        `).showImage(2)
+    images.createBigImage(`
+        # # # . . . . . . .
+        # . . . . . . . . .
+        # # # . # # . # # .
+        # . . . # . . # . .
+        # # # . # . . # . .
+        `).showImage(3)
+    images.createBigImage(`
+        # # # . . . . . . .
+        # . . . . . . . . .
+        # # # . # # . # # .
+        # . . . # . . # . .
+        # # # . # . . # . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        # . # # . . . . . .
+        . . # . . . . . . .
+        . . # . . . . . . .
+        `).showImage(0)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        # . # # . . . . . .
+        . . # . . . . . . .
+        . . # . . . . . . .
+        `).showImage(1)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        # . # # . . . . . .
+        . . # . . . . . . .
+        . . # . . . . . . .
+        `).showImage(2)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        # . # # . . . . . .
+        . . # . . . . . . .
+        . . # . . . . . . .
+        `).showImage(3)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        # . # # . . . . . .
+        . . # . . . . . . .
+        . . # . . . . . . .
+        `).showImage(4)
+    images.createBigImage(`
+        . . . . . . . . . .
+        . . . . . . . . . .
+        # . # # . . . . . .
+        . . # . . . . . . .
+        . . # . . . . . . .
+        `).showImage(5)
+}
+datalogger.onLogFull(function () {
+    basic.showString("Press A button to clear all data.")
+})
+input.onButtonPressed(Button.A, function () {
+    datalogger.deleteLog(datalogger.DeleteType.Full)
+})
+datalogger.log(datalogger.createCV("E", 0))
+Err()
+basic.clearScreen()
 basic.forever(function () {
 	
 })
